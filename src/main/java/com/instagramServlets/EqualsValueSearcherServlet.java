531df5257a125b64e64393d64ba39f9b069b716f
Ging123 <iceglauber@outlook.com>
2021-04-04 17:32:00 -0300

    add java files

---
+++ src/main/java/com/instagramServlets/EqualsValueSearcherServlet.java
@@ -0,0 +1,24 @@
+package com.instagramServlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import com.database.MySql;
+
+public class EqualsValueSearcherServlet extends HttpServlet {
+	
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    MySql sql = new MySql();
+    final boolean result = sql.hasEqualValue(request.getParameter("input"), request.getParameter("sqlColumn"));
+    PrintWriter out = response.getWriter();
+    ObjectMapper mapper = new ObjectMapper();
+    out.print(mapper.writeValueAsString(result));
+    out.flush();
+    out.close();
+  }
+}
